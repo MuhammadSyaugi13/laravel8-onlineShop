df7ec6ff11655f1af47c804e024976c0b9dd6805
MOSYQ <muhammadsyaugi13@gmail.com>
2021-10-18 16:54:27 +0700

    awal commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>@yield('title')</title>
+
+    {{-- style --}}
+    @stack('prepand-style')
+    @include('includes.style')
+    @stack('addon-style')
+
+  </head>
+
+  <body>
+    <!-- Navigation -->
+    {{-- navbar --}}
+    @include('includes.navbar-auth')
+
+    <!-- Page Content -->
+    {{-- content --}}
+    @yield('content')
+
+    {{-- footer --}}
+    @include('includes/footer')
+
+    <!-- Bootstrap core JavaScript -->
+    {{-- script --}}
+    @stack('prepand-script')
+    @include('includes.script')
+    @stack('addon-script')
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/debug-sentry', function () {
+//     throw new Exception('My first Sentry error!');
+// });
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
+Route::get('/detail/{id}', [App\Http\Controllers\DetailController::class, 'index'])->name('detail');
+Route::get('/cart', [App\Http\Controllers\CartController::class, 'index'])->name('cart');
+Route::get('/success', [App\Http\Controllers\CartController::class, 'success'])->name('success');
+Route::get('/register/success', [App\Http\Controllers\Auth\RegisterController::class, 'success'])->name('register-success');
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard-product', [App\Http\Controllers\DashboardProductController::class, 'index'])
+        ->name('dashboard-product');
+
+// Route::get('/', function () {
+//     return view('pages.home');
+// });
+
+
+Auth::routes();
+
